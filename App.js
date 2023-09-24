843812f31599876495194c2d8f69001cf0d5f6bd
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-09-24 15:44:53 +0530

    do assingment_1
    create a hello world program in react with cdn links

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading=React.createElement("h1",{},"Namaste React");
+
+const container=React.createElement("div",{},heading);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
