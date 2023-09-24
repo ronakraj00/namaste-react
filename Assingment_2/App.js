84ec10b1fc4ee2cf2018e90757a7740eeca7f06f
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-09-24 23:03:36 +0530

    do assingment_4

--- Assingment_2/App.js
+++ Assingment_2/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import RLogo from "./R-logo.svg"
+import userIcon from "./user-icon.jpg"
 
-const heading1 = React.createElement("h1", {}, "Namaste React");
-const heading2 = React.createElement("h2", {}, "I am Learning React.js");
-
-const container = React.createElement("div", {}, [heading1, heading2]);
-
+const Header = () => {
+    return (
+        <>
+            <header>
+                <div>
+                    <img src={RLogo} />
+                </div>
+                <div>
+                    <input type="search" placeholder="let's find" />
+                </div>
+                <div>
+                    <img src={userIcon} alt="user-icon" />
+                </div>
+            </header>
+        </>
+    );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(
+    <Header/>
+);
